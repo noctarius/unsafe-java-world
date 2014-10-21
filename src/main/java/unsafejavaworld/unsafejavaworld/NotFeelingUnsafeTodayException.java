b9245fdb7ba191e0b3c1cae97daf6335e8a4d82f
Yusuf Aytaş <yusufaytas@gmail.com>
2014-10-21 16:02:52 +0100

    Update NotFeelingUnsafeTodayException.java

--- src/main/java/unsafejavaworld/unsafejavaworld/NotFeelingUnsafeTodayException.java
+++ src/main/java/unsafejavaworld/unsafejavaworld/NotFeelingUnsafeTodayException.java
@@ -1,3 +1,9 @@
+// 
+// Dear Unsafe Ninja:
+// 
+// Once you are done trying everything with unsafe
+// feel free to throw this mighty exception.
+// 
 package unsafejavaworld.unsafejavaworld;
 
 public class NotFeelingUnsafeTodayException
